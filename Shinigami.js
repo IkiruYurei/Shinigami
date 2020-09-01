9fe56f653ed56e3f31a20bac35b82c41651c996b
IkiruYurei <70608456+IkiruYurei@users.noreply.github.com>
2020-09-01 17:57:15 -0400

    Add files via upload
    hhhhh idk what I'm doing

---
+++ Shinigami.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+const modules = ['Administration', 'Currency', 'Cursed', 'CustomCommands', 'Fun', 'Help', 'LevelingSystem', 'Messages', 'Misc', 'Moderation', 'Partner', 'REI', 'Searches', 'Staff'];
+
+
+
+modules.forEach(c => {
+    fs.readdir(`C:/Users/kawai/Documents/Shinigami/Commands/${c}/`, (err, files) => {
+        if (err) throw err;
+        console.log(`[Commandlogs] Loaded ${files.length} commands of module ${c}`);
+        files.forEach(f => {
+            const props = require(`./Commands/${c}/${f}`);
+            client.commands.set(props.help.name, props);
+        });
+    });
+});
+
+
+
+
+
+client.once('ready', () => {
+  console.log('Ready.');
+});
+
+client.on('message', msg => {
+    if (msg.author.id === client.user.id)
+        return;
+
+    if (msg.content === '?s yeetus') {
+        msg.reply('I have been summoned.');
+    }
+
+    if (msg.content === 'im in the ghetto') {
+        msg.channel.send('ratatata');
+    }
+});
+
+
+client.login(config.token);
